5d9b26b2b7e29074ecdb150e5f209f95fd2c00a6
natf17 <natfar.dev@gmail.com>
2023-07-11 07:58:02 -0400

    Add skeleton of first GraphQl integration test

--- src/test/java/com/ppublica/apps/kiosk/MyApplicationIT.java
+++ src/test/java/com/ppublica/apps/kiosk/MyApplicationIT.java
@@ -1,5 +1,6 @@
 package com.ppublica.apps.kiosk;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -7,6 +8,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.graphql.test.tester.HttpGraphQlTester;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -20,12 +22,36 @@ public class MyApplicationIT {
     @Autowired
     WebTestClient webTestClient;
 
+    @Autowired
+    HttpGraphQlTester graphqlTester;
+
+    @BeforeAll
+    static void setup() {
+        // populate about page with feature image width value 100
+
+    }
+
     @Test
     void exampleTest() {
         webTestClient.get().uri("/")
                       .exchange()
                       .expectStatus().isOk()
                       .expectBody(String.class).isEqualTo("Hello world!");
+    }
+
+    @Test
+    void exampleGraphQlTest() {
+        HttpGraphQlTester tester = graphqlTester.mutate()
+            .build();
+        tester.documentName("aboutPage")
+            .variable("locale", "en")
+            .execute()
+            .path("aboutPage", aboutPage -> aboutPage
+                                                .path("featImg.width")
+                                                .entity(Integer.class)
+                                                .isEqualTo(Integer.valueOf(100))
+            );
+
     }
 
 }
