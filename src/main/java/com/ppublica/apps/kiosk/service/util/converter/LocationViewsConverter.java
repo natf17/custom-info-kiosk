1413123fd0097e9840c20666b1538a7c6bf36e48
natf17 <natfar.dev@gmail.com>
2024-03-02 17:02:44 -0500

    Implemented LocationViewsConverterTest

--- src/main/java/com/ppublica/apps/kiosk/service/util/converter/LocationViewsConverter.java
+++ src/main/java/com/ppublica/apps/kiosk/service/util/converter/LocationViewsConverter.java
@@ -1,5 +1,65 @@
 package com.ppublica.apps.kiosk.service.util.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.ppublica.apps.kiosk.domain.model.kiosk.KioskImage;
+import com.ppublica.apps.kiosk.domain.model.kiosk.LocationType;
+import com.ppublica.apps.kiosk.service.views.LocalizedField;
+import com.ppublica.apps.kiosk.service.views.LocalizedView;
+import com.ppublica.apps.kiosk.service.views.locations.LocationAdminView;
+import com.ppublica.apps.kiosk.service.views.locations.LocationView;
+import com.ppublica.apps.kiosk.service.views.locations.MapImage;
+
+@Component
 public class LocationViewsConverter {
-    
+    public LocationView buildView(LocationType location) {
+        KioskImage map = location.map().fieldValue();
+        MapImage image = map != null ? new MapImage(map.width(), map.height(), map.location()): null;
+
+        return new LocationView(Long.toString(location.collectionId()), 
+                                location.fullName().fieldValue(), 
+                                location.level_num().fieldValue(),
+                                location.level_name().fieldValue(),
+                                image
+                                );
+    }
+
+    // expects at least one element in the list
+    public LocationAdminView buildAdminView(List<? extends LocationType> locations) {
+        LocationType arbitraryLocation = locations.get(0);
+
+        String id = arbitraryLocation.collectionId() != null ? Long.toString(arbitraryLocation.collectionId()): null;
+
+        List<LocalizedField> fullNameLoc = new ArrayList<>();
+        List<LocalizedField> levelNameLoc = new ArrayList<>();
+        List<LocalizedView<MapImage>> mapLoc = new ArrayList<>();
+
+        // process localizable fields
+        for (LocationType location : locations) {
+            String locale = location.kioskCollectionMetadata().getCollectionInternals().getKioskLocale();
+
+            String fullName = location.fullName().fieldValue();
+            if(fullName != null) {
+                fullNameLoc.add(new LocalizedField(locale, fullName));
+            }
+
+            String levelName = location.level_name().fieldValue();
+            if(levelName != null) {
+                levelNameLoc.add(new LocalizedField(locale, levelName));
+            }
+
+            KioskImage mapImage = location.map().fieldValue();
+            MapImage map = mapImage != null ? new MapImage(mapImage.width(), mapImage.height(), mapImage.location()): null;
+            mapLoc.add(new LocalizedView<MapImage>(locale, map));
+
+        }
+
+        // process non-locale fields
+        Integer levelNum = arbitraryLocation.level_num().fieldValue();
+
+        return new LocationAdminView(id, fullNameLoc, levelNum, levelNameLoc, mapLoc);
+    }
 }
---
+++ src/test/java/com/ppublica/apps/kiosk/service/util/converter/LocationViewsConverterTest.java
@@ -0,0 +1,92 @@
+package com.ppublica.apps.kiosk.service.util.converter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.ppublica.apps.kiosk.domain.model.kiosk.DefaultLocationType;
+import com.ppublica.apps.kiosk.domain.model.kiosk.KioskCollectionField;
+import com.ppublica.apps.kiosk.domain.model.kiosk.KioskCollectionMetadata;
+import com.ppublica.apps.kiosk.domain.model.kiosk.KioskImage;
+import com.ppublica.apps.kiosk.domain.model.kiosk.LocationType;
+import com.ppublica.apps.kiosk.domain.model.kiosk.Status;
+import com.ppublica.apps.kiosk.service.views.LocalizedField;
+import com.ppublica.apps.kiosk.service.views.LocalizedView;
+import com.ppublica.apps.kiosk.service.views.locations.LocationAdminView;
+import com.ppublica.apps.kiosk.service.views.locations.LocationView;
+import com.ppublica.apps.kiosk.service.views.locations.MapImage;
+
+public class LocationViewsConverterTest {
+    LocalDate testDate;
+    LocalDateTime testDateTime;
+    Long enLocaleId = 5L;
+
+    LocationType locationTypeEn;
+    LocationType locationTypeEs;
+    LocationView locationViewEn;
+    LocationView locationViewEs;
+    LocationAdminView locationAdminView;
+
+    LocationViewsConverter converter = new LocationViewsConverter();
+
+
+    @BeforeEach
+    public void setup() {
+        this.testDate = LocalDate.of(2024, 2, 27);
+        this.testDateTime = LocalDateTime.of(2024, 2, 28, 8, 30);
+
+        locationTypeEn = new DefaultLocationType.Builder()
+                                            .id(1L)
+                                            .collectionNameField(new KioskCollectionField<String>("Location", true))
+                                            .kioskCollectionMetadata(new KioskCollectionMetadata(enLocaleId, "en", Status.PUBLISHED, testDate, testDateTime))
+                                            .map(new KioskCollectionField<KioskImage>(new KioskImage("url", 1, 2), true))
+                                            .level_num(new KioskCollectionField<Integer>(6, false))
+                                            .fullName(new KioskCollectionField<String>("fullName_fieldValue", false))
+                                            .level_name(new KioskCollectionField<String>("levelName_fieldValue", true))
+                                            .build();
+
+        this.locationViewEn = new LocationView("1", "fullName_fieldValue", 6, "levelName_fieldValue", new MapImage(1, 2, "url"));
+
+        locationTypeEs = new DefaultLocationType.Builder()
+                                            .id(1L)
+                                            .collectionNameField(new KioskCollectionField<String>("Location", true))
+                                            .kioskCollectionMetadata(new KioskCollectionMetadata(2L, "es", Status.PUBLISHED, testDate, testDateTime))
+                                            .map(new KioskCollectionField<KioskImage>(new KioskImage("url_es", 1, 2), true))
+                                            .level_num(new KioskCollectionField<Integer>(6, false))
+                                            .fullName(new KioskCollectionField<String>("fullName_fieldValue_es", false))
+                                            .level_name(new KioskCollectionField<String>("levelName_fieldValue_es", true))
+                                            .build();
+
+
+
+        locationAdminView = new LocationAdminView("1",
+                                                    List.of(new LocalizedField("en", "fullName_fieldValue"),
+                                                            new LocalizedField("es", "fullName_fieldValue_es")),
+                                                    6,
+                                                    List.of(new LocalizedField("en", "levelName_fieldValue"),
+                                                            new LocalizedField("es", "levelName_fieldValue_es")),
+                                                    List.of(new LocalizedView<>("en", new MapImage(1, 2, "url")),
+                                                            new LocalizedView<>("es", new MapImage(1, 2, "url_es")))
+                                                );
+
+    }
+    
+    @Test
+    public void givenFirstAidType_buildView_returnsFirstAidView() {
+        LocationView viewActual = converter.buildView(locationTypeEn);
+
+        Assertions.assertEquals(locationViewEn, viewActual);
+    }
+
+    @Test
+    public void givenFirstAidTypeList_buildAdminView_returnsFirstAidAdminView() {
+        LocationAdminView adminViewActual = converter.buildAdminView(List.of(locationTypeEn, locationTypeEs));
+
+        Assertions.assertEquals(locationAdminView, adminViewActual);
+
+    }
+}
