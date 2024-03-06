8118acb98c6d5202856d4e99891aa02744b6761e
natf17 <natfar.dev@gmail.com>
2024-03-05 19:46:34 -0500

    Minor tweak

--- src/main/java/com/ppublica/apps/kiosk/domain/model/cms/collection/CollectionRelationship.java
+++ src/main/java/com/ppublica/apps/kiosk/domain/model/cms/collection/CollectionRelationship.java
@@ -1,3 +1,3 @@
-package com.ppublica.apps.kiosk.repository.collection;
+package com.ppublica.apps.kiosk.domain.model.cms.collection;
 
 public record CollectionRelationship(Long relatedEntityId, String type) {}
